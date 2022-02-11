6677138ddf4603972112ccaead83d28a4a8525de
Meena <meena0252.cse19@chitkara.edu.in>
2022-02-11 08:30:05 +0530

    fourth commit

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/todos');
+mongoose.connect('mongodb://localhost:27017/todos');
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, "Error in connecting to MongoDB"));
 
